8e60ed047b21cb525e596587b6cffd056930afd6
LolaBrt <lola.brouart@edu.univ-fcomte.fr>
2025-11-25 17:10:40 +0100

    tp fini

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 // *** ACCORDEON ***
+
+const summaries = document.querySelectorAll(".accordeon summary");
+
+summaries.forEach(summary => {
+    summary.addEventListener("click", (event) => {
+        const accordeon = document.querySelector(".accordeon");
+        accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+    })
+})
+
